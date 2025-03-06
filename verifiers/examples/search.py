64f6a32a6283c0b70b412ebea59ccecf5aa2eb2a
andthattoo <omer@firstbatch.xyz>
2025-03-06 23:55:13 +0300

    back to base

--- verifiers/examples/search.py
+++ verifiers/examples/search.py
@@ -2,7 +2,7 @@ import verifiers as vf
 from verifiers.tools import search, scrape
 from verifiers.prompts import SEARCH_FEW_SHOT
 
-model_name = "Qwen/Qwen2.5-7B-Instruct"
+model_name = "Qwen/Qwen2.5-7B"
 model, tokenizer = vf.get_model_and_tokenizer(model_name)
 
 vf_env = vf.ToolEnv(
@@ -21,9 +21,9 @@ training_args = vf.get_default_grpo_config(
     num_gpus=8 # Single GPU setup
 )
 # rollouts per prompt
-training_args.num_generations = 2  # Changed from 7 to 6 to make it evenly divide the batch size
+training_args.num_generations = 6  # Changed from 7 to 6 to make it evenly divide the batch size
 # minibatch size per GPU (bs 6 on single GPU / 6 rollouts -> 1 prompt per batch)
-training_args.per_device_train_batch_size = 2
+training_args.per_device_train_batch_size = 6
 # batches to accumulate (1 prompt * 4 -> 4 prompts per global batch)
 training_args.gradient_accumulation_steps = 4
 # steps per global batch (1 on-policy, 2 off-policy)
@@ -31,11 +31,11 @@ training_args.num_iterations = 2
 # no ref model
 training_args.beta = 0.0
 
-training_args.vllm_max_model_len = 32000
-training_args.vllm_gpu_memory_utilization=0.7
+#training_args.vllm_max_model_len = 64000
+#training_args.vllm_gpu_memory_utilization=0.7
 
-training_args.gradient_checkpointing = True
-training_args.bf16 = True
+#training_args.gradient_checkpointing = True
+#training_args.bf16 = True
 # training_args.max_grad_norm = 0.01
 
 trainer = vf.GRPOEnvTrainer(
